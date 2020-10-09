e677504925d5d2883673f06e543117dd2ea94839
Kumari Shivani Singh <kumarishivanisingh14@gmail.com>
2020-10-10 02:09:37 +0530

    Intro to middleware

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -4,17 +4,7 @@
 exports.getBootcamps = (req, res, next) => {
     res.status(200).json({
         success: true,
-        msg: 'Show all bootcamps '
-    });
-}
-
-// @desc       Get all bootcamps
-// @route      GET /api/v1/bootcamps
-// @access     Public
-exports.getBootcamps = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        msg: 'Show all bootcamps '
+        msg: 'Show all bootcamps'
     });
 }
 
